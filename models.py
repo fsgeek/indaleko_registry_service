98edceb60feca349c40424c63c5bf43c53851618
Tony Mason <tony@fsgeek@cs.ubc.ca>
2025-05-04 15:24:38 -0700

    Synchronize models between producer and consumer

--- models.py
+++ models.py
@@ -1,54 +1,116 @@
 # models.py
-
-from datetime import datetime
-from typing import Dict, Literal, Optional
+import enum
+from datetime import datetime, UTC
 from uuid import UUID
+from pydantic import BaseModel, AwareDatetime, field_validator
 
-from pydantic import BaseModel
+# -----------------------------
+# Helpers
+# -----------------------------
+
+
+def _validate_timestamp(
+    value: AwareDatetime,
+) -> AwareDatetime:
+    """Validate the timestamp."""
+    if value is None:
+        value = datetime.now(UTC)
+    if isinstance(value, AwareDatetime) or isinstance(value, datetime):
+        return value
+    ts = datetime.fromisoformat(value)
+    if not isinstance(ts, AwareDatetime):
+        raise ValueError(f"Timestamp {ts} is not an AwareDatetime.")
+    return ts
+
 
 # -----------------------------
 # API Models
 # -----------------------------
 
+
+class RegistrationCategory(enum.Enum):
+    ACTIVITY_PROVIDER = "activity_provider"
+    ANALYZER = "analyzer"
+    LLM = "llm"
+    MODEL_LABEL = "model_label"
+    SEMANTIC_LABEL = "semantic_label"
+    SEMANTIC_ATTRIBUTE = "semantic_attribute"
+    STORAGE_LAYER = "storage_layer"
+
+    @classmethod
+    def list(cls):
+        return [category.value for category in cls]
+
+
 class RegisterRequest(BaseModel):
-    category: Literal["activity_provider", "semantic_label", "analyzer", "llm"]
+    category: RegistrationCategory
     name: str
     description: str
-    schema: Optional[Dict] = None
-    cookie: Optional[Dict] = None
+    schema: dict | None = None
+    cookie: dict | None = None
+
 
 class RegisterResponse(BaseModel):
     uuid: UUID
-    registered_at: datetime
+    registered_at: AwareDatetime
+
+    @field_validator("registered_at", mode="before")
+    @classmethod
+    def validate_timestamp(
+        cls: "RegistryEntry",
+        value: AwareDatetime,
+    ) -> AwareDatetime:
+        return _validate_timestamp(value)
+
 
 class ResolveResponse(BaseModel):
     uuid: UUID
-    cookie: Optional[Dict] = None
+    cookie: dict | None = None
+
 
 class PropertiesRequest(BaseModel):
     uuid: UUID
     category: str
-    properties: Dict
-    cookie: Optional[Dict] = None
+    properties: dict
+    cookie: dict | None = None
+
 
 class PropertiesResponse(BaseModel):
     uuid: UUID
     category: str
-    properties: Dict
-    cookie: Optional[Dict]
-    updated_at: datetime
+    properties: dict | None
+    cookie: dict | None
+    updated_at: AwareDatetime
+
+    @field_validator("updated_at", mode="before")
+    @classmethod
+    def validate_timestamp(
+        cls: "RegistryEntry",
+        value: AwareDatetime,
+    ) -> AwareDatetime:
+        return _validate_timestamp(value)
+
 
 # -----------------------------
 # Internal Model
 # -----------------------------
+
 
 class RegistryEntry(BaseModel):
     uuid: UUID
     category: str
     name: str
     description: str
-    schema: Optional[Dict]
-    cookie: Optional[Dict]
+    schema: dict | None
+    cookie: dict | None
     version: int
-    registered_at: datetime
+    registered_at: AwareDatetime
     deprecated: bool
+
+    @field_validator("registered_at", mode="before")
+    @classmethod
+    def validate_timestamp(
+        cls: "RegistryEntry",
+        value: AwareDatetime,
+    ) -> AwareDatetime:
+        return _validate_timestamp(value)
